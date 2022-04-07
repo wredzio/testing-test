9ccb87dbc76f80f066130ba82f94121874853ebc
Mateusz <marchewka.matthew@gmail.com>
2022-04-07 12:40:12 +0200

    next things next

--- src/pages/tests/TestPage.tsx
+++ src/pages/tests/TestPage.tsx
@@ -12,6 +12,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { assertUnreachable } from "../../helpers/assertUnreachable";
 import { TaskViewer } from "./components/TaskViewer/TaskViewer";
+import { TestResult, TestResultCalculator } from "./helpers/TestResultCalculator/TestResultCalculator";
 import { getTest, TaskDto, TestDto } from "./TestPage.api";
 
 export interface TestPageProps {
@@ -19,13 +20,14 @@ export interface TestPageProps {
 }
 export interface TestViewProps {
   test: TestDto;
+  resultCalculator: TestResultCalculator;
 }
 
 export const TestView = (props: TestViewProps) => {
-  const { test } = props;
+  const { test, resultCalculator } = props;
   const [currentTask, setCurrentTask] = React.useState<TaskDto>(test.tasks[0]);
   const [answers, setAnswers] = React.useState(new Map<string, string>());
-  const [result, setResult] = React.useState<number>();
+  const [result, setResult] = React.useState<TestResult>();
 
   const updateAnswers = (task: TaskDto, answer: string) => {
     setAnswers((prev) => new Map([...prev, [task.id, answer]]));
@@ -74,7 +76,7 @@ export const TestView = (props: TestViewProps) => {
       }
     });
 
-    setResult(correctAnswersCount / test.tasks.length);
+    setResult(resultCalculator.calculate(correctAnswersCount, test.tasks.length))
   };
 
   const currentStepIndex = test.tasks.findIndex(
@@ -88,10 +90,19 @@ export const TestView = (props: TestViewProps) => {
   const isAnswered = currentTask ? !!answers.get(currentTask.id) : false;
 
   if (result !== undefined) {
-    <div>
-      <Heading as="h2">Test Finished!</Heading>
-      <p>Result: {result}</p>
-    </div>;
+    <VStack spacing={3}>
+      <Container maxW="container.sm">
+        <Box paddingTop={3} paddingBottom={3}>
+          <Heading as="h2">{result.title}</Heading>
+          <Text fontSize="md">
+            {result.description}
+          </Text>
+          <Text fontSize="md">
+            {`Has Passed: ${result.hasPassed}`}
+          </Text>
+        </Box>
+      </Container>
+    </VStack>
   }
 
   return (
@@ -154,5 +165,7 @@ export const TestPage = (props: TestPageProps) => {
     return assertUnreachable(data);
   }
 
-  return <TestView test={data} />;
+  const testResultCalculator = new TestResultCalculator(data.passThreshold, data.resultInfos);
+
+  return <TestView test={data} resultCalculator={testResultCalculator} />;
 };
